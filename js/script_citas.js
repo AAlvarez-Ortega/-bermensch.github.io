5333de83bf2564d8768923fa3b08e14d2a43c26f
AAlvarez-Ortega <154563188+AAlvarez-Ortega@users.noreply.github.com>
2024-01-06 16:33:13 -0600

    Update script_citas.js

--- js/script_citas.js
+++ js/script_citas.js
@@ -1,25 +1,19 @@
-const database = firebase.firestore();
 
-// Accede al formulario
+const database = firebase.firestore();
 const citaForm = document.getElementById('citaForm');
 
-// Agrega un evento al formulario para enviar datos a Firebase al hacer submit
-citaForm.addEventListener('submit', (e) => {
-  e.preventDefault(); // Evita el envío por defecto del formulario
-
-  // Obtén los valores del formulario
-  const nombre = document.getElementById('nombre').value;
-  const fecha = document.getElementById('fecha').value;
-  const contacto = document.getElementById('contacto').value;
-
-  // Crea un objeto con los datos
-  const Cita =  await database.collection('citas').doc().set({
-    nombre,
-    fecha,
-    contacto
-  })
-    console.log(Cita)
-    alert("cita agendada corretamente");
-
+function agendaCita() {
+    firestore.collection("Citas").add({
+      citaForm.addEventListener('submit', (e) => {
+      e.preventDefault(); // Evita el envío por defecto del formulario
   
-});
+      // Obtén los valores del formulario
+      const nombre = document.getElementById('nombre').value;
+      const fecha = document.getElementById('fecha').value;
+      const contacto = document.getElementById('contacto').value;
+      })
+    })
+  alert('Cita agendada con éxito');
+}
+
+
