7c13bd4b83256152c16cf49b821ce212a7b86a77
AAlvarez-Ortega <154563188+AAlvarez-Ortega@users.noreply.github.com>
2024-01-07 04:45:44 -0600

    Update script_citas_existentes.js

--- js/script_citas_existentes.js
+++ js/script_citas_existentes.js
@@ -6,15 +6,33 @@ db.collection("citas").get().then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
     const data = doc.data();
     const citaInfo = document.createElement('div');
-    citaInfo.innerHTML = `
-      <p><<img id="Avatar" alt="Avatar" src="${cod(data.avatar)}"></p>
-      <p>Contacto: ${data.contacto}</p>
-      <p>Fecha: ${data.fecha}</p>
-      <p>Hora de la cita: ${data.hora_de_cita}</p>
-      <p>Nombre: ${data.nombre}</p>
-      <p>Usuario: ${data.usuario}</p>
-      <hr>
-    `;
+    const avatarImg = document.createElement('img');
+    const listaDatos = document.createElement('ul');
+
+    avatarImg.src = data.avatar;
+    avatarImg.alt = 'Avatar';
+    avatarImg.width = 50; // Establece el ancho de la imagen
+
+    citaInfo.appendChild(avatarImg);
+
+    const contacto = document.createElement('li');
+    contacto.textContent = `Contacto: ${data.contacto}`;
+    const fecha = document.createElement('li');
+    fecha.textContent = `Fecha: ${data.fecha}`;
+    const horaCita = document.createElement('li');
+    horaCita.textContent = `Hora de la cita: ${data.hora_de_cita}`;
+    const nombre = document.createElement('li');
+    nombre.textContent = `Nombre: ${data.nombre}`;
+    const usuario = document.createElement('li');
+    usuario.textContent = `Usuario: ${data.usuario}`;
+
+    listaDatos.appendChild(contacto);
+    listaDatos.appendChild(fecha);
+    listaDatos.appendChild(horaCita);
+    listaDatos.appendChild(nombre);
+    listaDatos.appendChild(usuario);
+
+    citaInfo.appendChild(listaDatos);
     container.appendChild(citaInfo);
   });
 }).catch((error) => {
