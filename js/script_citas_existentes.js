2cb3a76e4748cebc5e3000209540f49dfcf691cf
AAlvarez-Ortega <154563188+AAlvarez-Ortega@users.noreply.github.com>
2024-01-07 04:27:55 -0600

    Update script_citas_existentes.js

--- js/script_citas_existentes.js
+++ js/script_citas_existentes.js
@@ -1,20 +1,22 @@
-    const db = firebase.firestore();
-    const container = document.getElementById('container');
+   const db = firebase.firestore();
+const container = document.getElementById('container');
 
-    // Consulta los datos en la colección específica
-    db.collection("citas").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        container.innerHTML = `
-          <p>Avatar: ${data.avatar}</p>
-          <p>Contacto: ${data.contacto}</p>
-          <p>Fecha: ${data.fecha}</p>
-          <p>Hora de la cita: ${data.hora_de_cita}</p>
-          <p>Nombre: ${data.nombre}</p>
-          <p>Usuario: ${data.usuario}</p>
-          <hr>
-        `;
-      });
-    }).catch((error) => {
-      console.error("Error al obtener los datos: ", error);
-    });
+// Consulta los datos en la colección específica
+db.collection("citas").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    const citaInfo = document.createElement('div');
+    citaInfo.innerHTML = `
+      <p>Avatar: ${data.avatar}</p>
+      <p>Contacto: ${data.contacto}</p>
+      <p>Fecha: ${data.fecha}</p>
+      <p>Hora de la cita: ${data.hora_de_cita}</p>
+      <p>Nombre: ${data.nombre}</p>
+      <p>Usuario: ${data.usuario}</p>
+      <hr>
+    `;
+    container.appendChild(citaInfo);
+  });
+}).catch((error) => {
+  console.error("Error al obtener los datos: ", error);
+});
